76f6a063618b47880dbb6dc0b2de3b0bf9d36631
gypogypo <crewteamtv@hotmail.com>
2017-02-13 19:31:16 +0000

    Update default.py

--- default.py
+++ default.py
@@ -3611,6 +3611,6 @@ if not viewmode==None:
 if xbmcvfs.exists(xbmc.translatePath('special://home/userdata/sources.xml')):
         with open(xbmc.translatePath('special://home/userdata/sources.xml'), 'r+') as f:
                 my_file = f.read()
-                if re.search(r'https://github.com/gypogypo/plugin.video.sneek/blob/master/Sneek.xml', my_file):
+                if re.search(r'https://github.com/gypogypo/plugin.video.sneek/blob/master/source_file', my_file):
                         xbmc.log('===Sneek===Source===Found===in===sources.xml===Not Deleting.===')
                
